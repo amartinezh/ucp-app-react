1e459312dee3ea43259d2fd3034f0dc36d326405
Andres Mauricio Martinez Hincapie <andresmauriciomartinezhincapie@MacBook-Air-de-Andres.local>
2025-05-17 11:16:54 -0500

    feat: Update config

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div>
       <h1>¡Universidad Católica de Pereira !</h1>
       <p>Listado de integrantes - Proceso de Desarrollo de Software I</p>
-      <p>profe: Andrés Mauricio</p>
+      <p>profe: Andrés Mauricio Martinez</p>
     </div>
   );
 }
