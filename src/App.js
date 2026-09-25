f465f6705153bb23d50b4f2a8bd214dd167c6906
Andres Mauricio Martinez Hincapie <amartinezh@gmail.com>
2026-09-24 19:12:52 -0500

    Trigger Jenkins

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Universidad Católica de Pereira !</h1>
+      <h1>¡Universidad Católica de Pereira !</h1>
       <h1>Especialización en Desarrollo de Software</h1>
       <p>Listado de integrantes - Proceso de Desarrollo de Software I</p>
       <p>profe: Andrés Mauricio Martinez Hincapie</p>
